cf7765c7412ec5fc788f04fba1fbed2487d8480a
dimmigg <dimmigg@gmail.com>
2017-10-05 11:53:42 +0300

    добавил календарь

--- app/src/main/java/com/dimka/myunivertable/CalendarActivity.java
+++ app/src/main/java/com/dimka/myunivertable/CalendarActivity.java
@@ -1,21 +1,59 @@
 package com.dimka.myunivertable;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import com.mikepenz.iconics.typeface.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Badgeable;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
 
 public class CalendarActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); // Отображение только портретный режим
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//ДОБАВИТЬ календарь и подпись четная/нечетная
+
+
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView1);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year,
+                                            int month, int dayOfMonth) {
+                int mYear = year;
+                int mMonth = month;
+                int mDay = dayOfMonth;
+
+
+
+//                String selectedDate = new StringBuilder().append(mMonth + 1)
+//                        .append("-").append(mDay).append("-").append(mYear)
+//                        .append(" ").toString();
+//                Toast.makeText(getApplicationContext(), selectedDate, Toast.LENGTH_LONG).show();
+
+
+            }
+        });
     }
 
 }
